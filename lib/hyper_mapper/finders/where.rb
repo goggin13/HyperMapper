7f4a763fd84a9fa1eb7ca76fc20e5017e91e73c9
goggin13 <goggin13@gmail.com>
2013-02-16 16:06:30 -0500

    find function

--- lib/hyper_mapper/finders/where.rb
+++ lib/hyper_mapper/finders/where.rb
@@ -2,6 +2,13 @@
 module HyperMapper
   module Finders 
     
+    def find(key)
+      result = HyperMapper::Config.client.get(space_name, key)
+      return load_from_attrs(result) if result
+
+      nil
+    end
+
     def where(params={})
       results = HyperMapper::Config.client.search(space_name, params)
       results.map { |r| load_from_attrs(r) }
--- spec/hyper_mapper/finders/where_spec.rb
+++ spec/hyper_mapper/finders/where_spec.rb
@@ -12,15 +12,15 @@ describe 'HyperMapper::Document::Finders' do
                                         field_2: "hello"
   end
 
+  before do
+    @client = stub_client
+  end
+
   after :all do
     Object.send(:remove_const, :FinderTestClass)
   end
   
   describe "where" do
-    
-    before do
-      @client = stub_client
-    end
     
     describe "with results" do
       
@@ -50,6 +50,33 @@ describe 'HyperMapper::Document::Finders' do
                .and_return([])      
         results = FinderTestClass.where field_2: "hello"
         results.should == []
+      end
+    end
+  end
+
+  describe "find" do
+
+    describe "with results" do
+      
+      it "should return the matching object" do
+        @client.should_receive(:get)
+               .with('finder_test_classes', 'test')
+               .and_return({field_1: "test", field_2: "hello"})
+        result = FinderTestClass.find('test')
+        result.should be_a FinderTestClass
+        result.field_1.should == 'test'
+        result.field_2.should == 'hello'
+      end
+    end
+
+    describe "without results" do
+
+      it "should return nil" do
+        @client.should_receive(:get)
+               .with('finder_test_classes', 'test')      
+               .and_return(nil)
+        result = FinderTestClass.find('test')
+        result.should be_nil
       end
     end
   end
